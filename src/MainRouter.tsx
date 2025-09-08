77bfdf379beafc2c0e9c6d71b4acc52681af8892
jordan582 <jordan@loanpros.io>
2025-09-08 11:37:50 -0700

    Update MainRouter.tsx

--- src/MainRouter.tsx
+++ src/MainRouter.tsx
@@ -2,6 +2,19 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 
 function MainRouter() {
+  // Check if we're on a subdomain that should serve static files
+  const hostname = window.location.hostname;
+  const isStaticSubdomain = hostname.startsWith('start.') || 
+                           hostname.startsWith('book.') || 
+                           hostname.startsWith('thanks.');
+
+  // If we're on a subdomain, don't render React Router
+  // This allows Netlify redirects to work properly
+  if (isStaticSubdomain) {
+    return null;
+  }
+
+  // Normal React routing for main domain
   return (
     <BrowserRouter>
       <Routes>
